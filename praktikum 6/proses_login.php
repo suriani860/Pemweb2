c67b58eb4c39293c99d581d7c2d83a44e55a3ceb
suriani860 <146050844+suriani860@users.noreply.github.com>
2024-04-24 14:10:12 +0700

    Add files via upload

---
+++ praktikum 6/proses_login.php
@@ -0,0 +1,11 @@
+<?php
+// tangkap input login
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// validasi login
+if ($email == 'admin@gmail.com' && $password == '12345678') {
+    header('Location: dashboard.php');
+} else {
+    echo "Login Gagal";
+}
